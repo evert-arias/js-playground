300b8f6a21fc3958764d549657e7f316f9dc305f
Evert Arias <evert.arias@aegis-ps.com>
2022-03-12 12:30:30 -0500

    Use array filter methods

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
-import { filter } from "./array.js";
+import { getEmojis, getFemaleUsers, getUsersByAge, getUsersByGenderAndAge } from "./array.js";
+import { Gender } from "./types/index.js";
 
-console.log('Filter: ', filter())
+console.log('Filter emojis: ', getEmojis())
+
+console.log('Get all female users: ', getFemaleUsers())
+
+console.log('Get users by age: ', getUsersByAge(10))
+
+console.log('Get users by gender and age: ')
+
+console.table(getUsersByGenderAndAge(Gender.FEMALE, 14))
