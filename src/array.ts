c505b8d4375799fecc9a0a95840e84b266ab046c
Evert Arias <evert.arias@aegis-ps.com>
2022-03-12 13:02:27 -0500

    Print methods result inside array.js

--- src/array.ts
+++ src/array.ts
@@ -1,18 +1,28 @@
 import { emojis, users } from './data/index.js'
 import { Gender, IUser } from './types/general.js'
 
-export const getEmojis = () => {
+const getEmojis = () => {
   return emojis.filter((emoji) => emoji >= '🐶')
 }
 
-export const getFemaleUsers = () => {
+const getFemaleUsers = () => {
   return users.filter((user: IUser) => user.gender === 'female')
 }
 
-export const getUsersByAge = (age: number) => {
+const getUsersByAge = (age: number) => {
   return users.filter((user: IUser) => { return user.age >= age })
 }
 
-export const getUsersByGenderAndAge = (gender: Gender, age: number) => {
+const getUsersByGenderAndAge = (gender: Gender, age: number) => {
   return users.filter((user: IUser) => { return (user.gender === gender && user.age >= age) })
 }
+
+const run = () => {
+  console.log('Filter emojis: ', getEmojis())
+  console.log('Get all female users: ', getFemaleUsers())
+  console.log('Get users by age: ', getUsersByAge(10))
+  console.log('Get users by gender and age: ')
+  console.table(getUsersByGenderAndAge(Gender.FEMALE, 14))
+}
+
+export { run };
