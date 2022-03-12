c30e15e9a15e59aebb904ac3c76aef3ea8b7818a
Evert Arias <evert.arias@aegis-ps.com>
2022-03-12 12:31:14 -0500

    Add interfaces and enums

---
+++ src/types/general.ts
@@ -0,0 +1,21 @@
+export interface IUser {
+    id: number;
+    name: string;
+    username: string;
+    gender: string;
+    age: number;
+}
+
+export interface IProduct {
+    id: string;
+    name: string;
+    price: number;
+    storage: number;
+    type: string;
+    size: string;
+}
+
+export enum Gender {
+    MALE = "male",
+    FEMALE = "female"
+}
