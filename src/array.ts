5574ede27e87466f1443e168d20572d070e2b364
Evert Arias <evert.arias@aegis-ps.com>
2022-03-12 12:30:17 -0500

    Add array filter methods

--- src/array.ts
+++ src/array.ts
@@ -1,6 +1,18 @@
-import { emojis } from './data/index.js'
+import { emojis, users } from './data/index.js'
+import { Gender, IUser } from './types/general.js'
 
-export const filter = () => {
+export const getEmojis = () => {
   return emojis.filter((emoji) => emoji >= '🐶')
 }
 
+export const getFemaleUsers = () => {
+  return users.filter((user: IUser) => user.gender === 'female')
+}
+
+export const getUsersByAge = (age: number) => {
+  return users.filter((user: IUser) => { return user.age >= age })
+}
+
+export const getUsersByGenderAndAge = (gender: Gender, age: number) => {
+  return users.filter((user: IUser) => { return (user.gender === gender && user.age >= age) })
+}
